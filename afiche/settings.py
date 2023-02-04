926fe7bf0e95d6bf8c6ee9812191d5b0431a2d3c
skantrop <akimbaeva.a23@gmail.com>
2023-02-04 13:59:44 +0600

    CORS confs add

--- afiche/settings.py
+++ afiche/settings.py
@@ -153,13 +153,16 @@ CORS_ALLOW_ORIGINS = [
     'http://127.0.0.1:3000',
     'http://127.0.0.1:3001', 
     'https://www.thunderclient.com',
+    'https://afiche-production.up.railway.app'
 ]
+
 CORS_ORIGIN_WHITELIST = [
     'http://localhost:3000',
     'http://localhost:3001', 
     'http://127.0.0.1:3000',
     'http://127.0.0.1:3001', 
     'https://www.thunderclient.com',
+    'https://afiche-production.up.railway.app',
 ]
 
 from datetime import timedelta
