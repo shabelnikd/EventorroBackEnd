592e052e8425188ff94982d3f63a31944674d93f
skantrop <akimbaeva.a23@gmail.com>
2023-02-04 14:18:53 +0600

    scrf

--- afiche/settings.py
+++ afiche/settings.py
@@ -197,3 +197,6 @@ EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
 EMAIL_PORT = config('EMAIL_PORT', cast=int)
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+
+
+CSRF_TRUSTED_ORIGINS=['https://afiche-production.up.railway.app']
