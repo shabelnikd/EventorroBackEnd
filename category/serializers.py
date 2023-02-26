999fab6959415a0fae36f91242b5b9e79962e219
skantrop <akimbaeva.a23@gmail.com>
2023-02-26 18:29:40 +0600

    category changes done

--- category/serializers.py
+++ category/serializers.py
@@ -4,18 +4,20 @@ from category.models import Category
 class SubCategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        fields = ('name', )
+        exclude = ('parent', )
 
 class CategoryListSerializer(serializers.ModelSerializer):
     # children = serializers.SerializerMethodField()
 
     class Meta:
         model = Category
-        fields = ('name', )
+        exclude = ('parent', )
+
 
 
     def to_representation(self, instance):
         rep = super().to_representation(instance)
-        rep['sub'] = SubCategorySerializer(instance.children.all(), many=True).data
+        subcategory = SubCategorySerializer(instance.children.all(), many=True).data
+        if subcategory != []:
+            rep['sub'] = subcategory
         return rep
-
--- category/urls.py
+++ category/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from category.views import (CategoryListView, CategoryDetailView)
 urlpatterns = [
     path('', CategoryListView.as_view()),
-#     path('<str:slug>/', CategoryDetailView().as_view()),
+    path('<str:slug>/', CategoryDetailView().as_view()),
 ]
