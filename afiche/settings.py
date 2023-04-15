43f68736c43bc6ce78d63d6d19a76c2d3f6f8497
skantrop <akimbaeva.a23@gmail.com>
2023-04-16 02:20:22 +0600

    server crush logs

--- afiche/settings.py
+++ afiche/settings.py
@@ -148,10 +148,8 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
-    'SITE_SCHEME': 'https',
 }
 
-SECURE_SSL_REDIRECT = True
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
 # CORS_ORIGIN_ALLOW_ALL = True
