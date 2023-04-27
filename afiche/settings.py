6af21c28d0be5374995503f495bddecfe1ccf735
skantrop <akimbaeva.a23@gmail.com>
2023-04-28 00:41:32 +0600

    pagination deleted

--- afiche/settings.py
+++ afiche/settings.py
@@ -143,8 +143,8 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 
 REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 6,
+    # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    # 'PAGE_SIZE': 6,
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
